e6e0be70f6b0e99422bd0e62d5fc4eb597851013
Nikodem <n.lu@onet.pl>
2026-02-12 15:40:55 +0100

    v1.0.1

--- source/states/State.cpp
+++ source/states/State.cpp
@@ -1,18 +1,8 @@
 #include "State.hpp"
 
-void State::render()
-{
-
-}
-
 State::State(sf::RenderWindow & window, sf::Font & font,
 std::stack <std::unique_ptr <State>> & states) 
 : window(window), font(font), states(states)
 {
 
-}
-
-void State::update(sf::Vector2f && mousePosition)
-{
-	
 }
